8b47065d6a12e1f0ab1b830864bb065c1c717592
AashiqinCode <aashiqahmed10101@ggmail.com>
2020-06-29 22:47:53 +0530

    ch#4-HTTP-Completed

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       home: Homepage(),
       theme: ThemeData(
         primarySwatch: Colors.blue,
-
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
-
 import '../drawer.dart';
-import '../name_card_widget.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Homepage extends StatefulWidget {
-
-
   @override
   _HomepageState createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
   var url = "http://jsonplaceholder.typicode.com/photos";
-
-  var myText = "Change my name";
-  TextEditingController _nameController = TextEditingController();
-
+  var data;
   // Used to initialize something before starting of the screen
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    fetchdata();
   }
 
   @override
@@ -29,50 +25,46 @@ class _HomepageState extends State<Homepage> {
     super.dispose();
   }
 
+  fetchdata() async {
+    var res = await http.get(url);
+    // print(res.body);
+
+    // json parsing
+    data = jsonDecode(res.body);
+    print(data);
+
+    //To tell the UI that we got the data
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   color: Colors.blue,
-    //   child: Text("Hi Flutter App"),
-    // );
-
     //Scaffold has prebuild some widget themes
-     return Scaffold(
+    return Scaffold(
       backgroundColor: Colors.green[100],
       appBar: AppBar(
         title: Text("My App"),
       ),
 
       // Container is similiar to <Div>
-      body: Center(
-        // use child: Row for changes
-        // child: Container(
-        //   height: 100,
-        //   width: 100,
-        //   color: Colors.teal,
-        // ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
+      body: data != null
+          // ? Container(
+          //     color: Colors.white,
+          // )
+          ? ListView.builder(itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(data[index]["title"]),
+                subtitle: Text("ID :${data[index]["id"]}"),
+                leading: Image.network(data[index]["url"]),
+              );
+            })
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
 
-          // Scrollable view
-          child: SingleChildScrollView(
-            //  First I pressed the bulb icon and choosed extract by widget option
-            child:
-                NameCardWidget(myText: myText, nameController: _nameController),
-          ),
-        ),
-      ),
       drawer: MyDrawer(),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          myText = _nameController.text;
-
-          setState(() {});
-
-          // Clears the text field after button press
-          _nameController.clear();
-        },
         child: Icon(Icons.send),
         hoverColor: Colors.black,
       ),
